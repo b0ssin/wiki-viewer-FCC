b78445acd3759cebde0b1e964dce8f331d76c1fb
= <krillinmaster@gmail.com>
2017-01-08 13:37:14 -0800

    Initial commit after a couple days of working the project. Git commits will start to begin from the start of the project as to share with my other computers

---
+++ wiki-viewer.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	console.log('ok')
+	$('button').click(function() {
+		console.log('click function working');
+/*		
+		var searchTerms = $("#search-bar").val();
+		console.log(searchTerms);
+		var url = "https;//en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerms + "&format-json&callback=?";
+		//ajax call
+		console.log('pre');
+		$.ajax({
+			type: "GET",
+			url: url,
+			dataType: "json",
+			success: function(data) {
+				alert('success');
+			},
+			error: function(errorMessage) {
+				alert(errorMessage);
+			}
+		});
+
+	});*/
+
+	});
+});
